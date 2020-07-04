4de60d6e12d74ad45dd73fb02da690b475205384
Igor <igormcsouza@gmail.com>
2020-07-04 11:43:47 -0300

    added sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,33 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <m-side-bar />
+    <v-main>
+      <v-card height="500" class="overflow-hidden main-card">
+        <router-view></router-view>
+      </v-card>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import MSideBar from "./components/SideBar";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    MSideBar
   },
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
+
+<style scoped>
+.main-card {
+  margin: 10px 50px;
+  padding: 30px;
+}
+</style>
